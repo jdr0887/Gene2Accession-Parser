7b89ad3acc1b105aed16feaa5328d64c5dce35ab
Jason Reilly <jdr0887@gmail.com>
2015-07-06 14:53:04 -0400

    initial commit

---
+++ src/main/java/org/renci/gene2accession/filter/G2ARNANucleotideAccessionVersionPrefixFilter.java
@@ -0,0 +1,27 @@
+package org.renci.gene2accession.filter;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.renci.gene2accession.G2AFilter;
+import org.renci.gene2accession.model.Record;
+
+public class G2ARNANucleotideAccessionVersionPrefixFilter implements G2AFilter {
+
+    private List<String> prefixList;
+
+    public G2ARNANucleotideAccessionVersionPrefixFilter(List<String> prefixList) {
+        super();
+        this.prefixList = prefixList;
+    }
+
+    @Override
+    public boolean accept(Record record) {
+        if (StringUtils.isNotEmpty(record.getRNANucleotideAccessionVersion())
+                && prefixList.contains(record.getRNANucleotideAccessionVersion().substring(0, 3))) {
+            return true;
+        }
+        return false;
+    }
+
+}
