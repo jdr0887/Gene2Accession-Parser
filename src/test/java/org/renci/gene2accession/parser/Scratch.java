c024f25c2b20cd2464c2ca8077a02aa2bd8c4cd8
Jason Reilly <jdr0887@gmail.com>
2015-07-06 14:53:17 -0400

    adding filter

--- src/test/java/org/renci/gene2accession/parser/Scratch.java
+++ src/test/java/org/renci/gene2accession/parser/Scratch.java
@@ -11,6 +11,7 @@ import org.renci.gene2accession.G2AFilter;
 import org.renci.gene2accession.G2AParser;
 import org.renci.gene2accession.filter.G2AAndFilter;
 import org.renci.gene2accession.filter.G2AAssemblyFilter;
+import org.renci.gene2accession.filter.G2ARNANucleotideAccessionVersionPrefixFilter;
 import org.renci.gene2accession.filter.G2ATaxonIdFilter;
 import org.renci.gene2accession.model.Record;
 
@@ -20,7 +21,8 @@ public class Scratch {
     public void testFilterAssembly() {
         G2AParser parser = G2AParser.getInstance(16);
         G2AAndFilter andFilter = new G2AAndFilter(Arrays.asList(new G2AFilter[] { new G2ATaxonIdFilter(9606),
-                new G2AAssemblyFilter("Reference GRCh38.p2 Primary Assembly") }));
+                new G2AAssemblyFilter("Reference GRCh38.p2 Primary Assembly"),
+                new G2ARNANucleotideAccessionVersionPrefixFilter(Arrays.asList(new String[] { "NM_", "NR_" })) }));
         List<Record> recordList = parser.parse(andFilter, new File("/tmp", "gene2refseq.gz"));
         assertTrue(recordList != null && !recordList.isEmpty());
         for (Record record : recordList) {
